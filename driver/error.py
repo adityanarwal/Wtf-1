dc93ef3e992c09d6dede49ec7a07d127a4a4d1b7
adityanarwal <93639647+adityanarwal@users.noreply.github.com>
2022-04-13 18:26:08 +0530

    Update error.py

--- driver/error.py
+++ driver/error.py
@@ -99,14 +99,14 @@ class nexaub:
 **Command:** `{Config.CMD_PREFIX + command[0]}`
 **Traceback:**
 `{e}`
-**Forward this to @NexaUB_Support**
+**Forward this to @Its_romeoo**
 """
                     if len(error_text) > 4000:
                         clean_error_text = await rm_markdown(error_text)
                         file = open("error_nexaub.txt", "w+")
                         file.write(clean_error_text)
                         file.close()
-                        await NEXAUB.send_document(LOG_CHANNEL_ID, "error_nexaub.txt", caption="`Error of Nexa Userbot`")
+                        await NEXAUB.send_document(LOG_CHANNEL_ID, "error_nexaub.txt", caption="𝐄𝐫𝐫𝐨𝐫 𝟒𝟎𝟒 !! Something went wrong, Please wait for developers to Fix It.")
                         os.remove("error_nexaub.txt")
                     else:
                         await NEXAUB.send_message(chat_id=LOG_CHANNEL_ID, text=error_text)
@@ -137,14 +137,14 @@ class nexaub:
 **Command:** `(Noting, Custom Filter)`
 **Traceback:**
 `{e}`
-**Forward this to @NexaUB_Support**
+**Forward this to @Its_romeoo**
 """
                     if len(error_text) > 4000:
                         clean_error_text = await rm_markdown(error_text)
                         file = open("error_nexaub.txt", "w+")
                         file.write(clean_error_text)
                         file.close()
-                        await NEXAUB.send_document(LOG_CHANNEL_ID, "error_nexaub.txt", caption="`Error of Nexa Userbot`")
+                        await NEXAUB.send_document(LOG_CHANNEL_ID, "error_nexaub.txt", caption="𝐄𝐫𝐫𝐨𝐫 𝟒𝟎𝟒 !! Something went wrong, Please wait for developers to Fix It.")
                         os.remove("error_nexaub.txt")
                     else:
                         await NEXAUB.send_message(chat_id=LOG_CHANNEL_ID, text=error_text)
